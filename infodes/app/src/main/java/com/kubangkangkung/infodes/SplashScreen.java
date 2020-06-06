f9b2210913cf854aa23f0124c8e24ef2b42308d0
misbachulmunir <misbachulmunir29052gmail.com>
2020-06-06 13:48:27 +0700

    Master

---
+++ infodes/app/src/main/java/com/kubangkangkung/infodes/SplashScreen.java
@@ -0,0 +1,25 @@
+package com.kubangkangkung.infodes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        Handler handle =new Handler();
+        handle.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent pindah=new Intent(SplashScreen.this,MainActivity.class);
+                startActivity(pindah);
+                finish();
+            }
+        },2000);
+    }
+}
